9c880cbc9503c2dc5f4e48b6cfb8395521ae177a
DivineDris <sava200104@gmail.com>
2025-11-17 03:01:11 +0200

    my final commit

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -28,9 +28,11 @@ public class Flower : MonoBehaviour, IInteractable
     public FlowerType flowerType = FlowerType.NotDefined;
     public RegionType regionType = RegionType.NotDefined;
     public FertilizerType fertilizerType = FertilizerType.NotDefined;
-    
-    [SerializeField]
-    private GameManager gameManager;
+
+    public Mesh[] meshes;
+    public Material[] materialsPink;
+    public Material[] materialsSky;
+    public Material[] materialsGreen;
 
     [SerializeField]
     private GameObject Body;
@@ -42,22 +44,54 @@ public class Flower : MonoBehaviour, IInteractable
 
     private void Start()
     {
-        gameManager = FindAnyObjectByType<GameManager>();
         Body = transform.Find("PlantBody").gameObject;
     }
+    public (Mesh, Material) GetModel(FlowerType flowerType, RegionType regionType)
+    {
+        Mesh mesh;
+        Material[] materialPool;
+        switch (flowerType)
+        {
+            case FlowerType.Tulip:
+                mesh = meshes[0];
+                materialPool = materialsPink;
+                break;
+            case FlowerType.Echivel:
+                mesh = meshes[1];
+                materialPool = materialsSky;
+                break;
+            case FlowerType.LightFlower:
+                mesh = meshes[2];
+                materialPool = materialsGreen;
+                break;
+            default:
+                mesh = meshes[0];
+                materialPool = materialsPink;
+                break;
+        }
 
+        switch (regionType)
+        {
+            case RegionType.LushGreenPlain:
+                return (mesh, materialPool[0]);
+            case RegionType.Іcy:
+                return (mesh, materialPool[1]);
+            case RegionType.Desert:
+                return (mesh, materialPool[2]);
+            default:
+                return (mesh, materialPool[0]);
+        }
+    }
     public void SpawnSlime()
     {
-        if(gameManager != null)
-        {
             Vector3 position = this.transform.position;
             var go = Instantiate(slimePrefab, position, Quaternion.identity);
             var slime = go.GetComponent<Slime>();
 
-            var model = gameManager.GetModel(flowerType,regionType);
+            var model = GetModel(flowerType,regionType);
             slime.Setup(model.Item1, model.Item2);
 
-        }
+        Debug.Log("Slime prefab at runtime = " + slimePrefab);
     }
     public void OnInteraction(PlayerController interactor)
     {
--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -2,20 +2,22 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour, IInteractable
 {
-    [SerializeField]
     MeshFilter meshFilter;
-    [SerializeField]
     MeshRenderer meshRenderer;
 
     private void Awake()
     {
         meshFilter = GetComponent<MeshFilter>();
+        Debug.Log("Mesh Filter: " + meshFilter);
         meshRenderer = GetComponent<MeshRenderer>();
+        Debug.Log("Mesh Renderer: " + meshRenderer);
+        Debug.Log("Spawned slime at" + transform.position);
     }
     public void Setup(Mesh mesh, Material material)
     {
         meshFilter.mesh = Instantiate(mesh);
         meshRenderer.material = material;
+        Debug.Log($"Slime Mesh: {meshFilter.mesh}, Material: {meshRenderer.material}");
     }
     public void OnInteraction(PlayerController interactor)
     {
